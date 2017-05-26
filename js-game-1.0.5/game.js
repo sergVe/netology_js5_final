bc58c19b4102c036f16ceabd613b4b4276a68345
Serg Ve <testfrontol@gmail.com>
2017-05-26 20:29:57 +0300

    Закончил писать класс Actor в game.js

--- js-game-1.0.5/game.js
+++ js-game-1.0.5/game.js
@@ -7,19 +7,15 @@ class Vector {
   }
 
   plus(addedVector) {
-    try {
+
       if (!(addedVector instanceof Vector)) {
         throw `Можно прибавлять к вектору только вектор типа Vector`;
       }
       return new Vector(this.x + addedVector.x, this.y + addedVector.y);
-    }
-    catch (e) {
-      console.log(e);
-    }
   }
 
   times(multiplier) {
-    try {
+
       if (typeof multiplier !== 'number') {
         throw `В качестве множителя должно быть число`;
       }
@@ -29,10 +25,6 @@ class Vector {
 
         return new Vector(this.x, this.y);
       }
-    }
-    catch (e) {
-      console.log(e);
-    }
   }
 }
 
@@ -50,50 +42,114 @@ class Vector {
 
 class Actor {
   constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
-    try {
-      //console.log([...arguments]);
-      if( [...arguments].length > 0 && [...arguments].some((item) => !(item instanceof Vector))) {
-        throw `Все аргументы должны быть объектами типа Vector`;
-      }
-      this.pos = new Vector(pos.x, pos.y);
-      this.size = new Vector(size.x, size.y);
-      this.speed = new Vector(speed.x, speed.y);
-      this.act = function () {};
-
-      Object.defineProperty(this, 'type', {
-        value: 'actor'
-      });
-      Object.defineProperty(this, 'left', {
-        value: this.pos.x
-      });
-      Object.defineProperty(this, 'top', {
-        value: this.pos.y
-      });
-      Object.defineProperty(this, 'right', {
-        value: this.pos.x + this.size.x
-      });
-      Object.defineProperty(this, 'bottom', {
-        value: this.pos.y + this.size.y
-      });
-      }
-
-    catch(e) {
-      console.log(e);
+    //console.log([...arguments]);
+    if (arguments.length > 0 && [...arguments].some((item) => !(item instanceof Vector))) {
+      throw `Все аргументы должны быть объектами типа Vector`;
     }
+
+    this.pos = new Vector(pos.x, pos.y);
+    this.size = new Vector(size.x, size.y);
+    this.speed = new Vector(speed.x, speed.y);
+
+    this.act = function () {
+    };
+
+    Object.defineProperty(this, 'type', {
+      value: 'actor'
+    });
+    Object.defineProperty(this, 'left', {
+      get() {
+        return this.pos.x;
+      }
+    });
+    Object.defineProperty(this, 'top', {
+      get() {
+        return this.pos.y;
+      },
+      enumerable: true
+    });
+    Object.defineProperty(this, 'right', {
+      get() {
+        return this.pos.x + this.size.x;
+      },
+      enumerable: true
+    });
+    Object.defineProperty(this, 'bottom', {
+      get() {
+        return this.pos.y + this.size.y;
+      },
+      enumerable: true
+
+    });
+
   }
+
+
   isIntersect(actorInstance) {
-    try {
-      if ([... arguments].length === 0 || !(actorInstance instanceof Actor)) {
-        throw `Вы не передали аргумент типа Actor`;
-      }
-      if (this === actorInstance) {
-        return false;
-      }
 
+    const isHalfIntersect = (transferredObject, thisObject, p1, p2, p3, p4) => {
+      if ((transferredObject[p1] >= thisObject[p1] && transferredObject[p1] <= thisObject[p2])
+        && (transferredObject[p3] >= thisObject[p3] && transferredObject[p3] <= thisObject[p4] ||
+        transferredObject[p4] >= thisObject[p3] && transferredObject[p4] <= thisObject[p4])) {
+        //console.log(`yes`);
+        return true;
+      }
+      //console.log(`no`, this[p1], actorInstance[p1]);
+      return false;
+    };
 
+    if (arguments.length === 0 || !(actorInstance instanceof Actor)) {
+      throw `Вы не передали аргумент типа Actor`;
     }
-    catch(e) {
-      console.log(e);
+    if (this === actorInstance) {
+      return false;
+    }
+    else {
+      return isHalfIntersect(actorInstance, this, 'left', 'right', 'top', 'bottom')
+        || isHalfIntersect(actorInstance, this,
+          'right', 'left', 'top', 'bottom');
+    }
+
+  }
+}
+
+
+const items = new Map();
+const player = new Actor();
+items.set('Игрок', player);
+items.set('Первая монета', new Actor(new Vector(10, 10)));
+items.set('Вторая монета', new Actor(new Vector(15, 5)));
+
+function position(item) {
+  return ['left', 'top', 'right', 'bottom']
+    .map(side => `${side}: ${item[side]}`)
+    .join(', ');
+}
+
+function movePlayer(x, y) {
+  player.pos = player.pos.plus(new Vector(x, y));
+}
+
+function status(item, title) {
+  console.log(`${title}: ${position(item)}`);
+  if (player.isIntersect(item)) {
+    console.log(`Игрок подобрал ${title}`);
+  }
+}
+
+/*items.forEach(status);
+ movePlayer(10, 10);
+ //console.log(player);
+ items.forEach(status);
+ movePlayer(5, -5);
+ items.forEach(status);*/
+
+// ***************************************************8
+
+class Level {
+  constructor(gridMatrix = [], movingObjectsArray = []) {
+    if (arguments.length > 0 && [...arguments].some(item => !Array.isArray(item))) {
+      throw `game over`;
     }
 
   }
@@ -101,5 +157,3 @@ class Actor {
 
 
 
-const a = new Actor(new Vector(1, 3));
-console.log(a);
