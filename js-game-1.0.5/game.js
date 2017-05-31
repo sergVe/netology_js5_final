21e0e384edf882d35717390261e86d21db8b2e3d
Serg Ve <testfrontol@gmail.com>
2017-05-31 20:27:30 +0300

    Закончил тестировать классы Fireball, HorizontalFireball, VerticalFireball, FireRain  в game.js

--- js-game-1.0.5/game.js
+++ js-game-1.0.5/game.js
@@ -9,40 +9,24 @@ class Vector {
   plus(addedVector) {
 
     if (!(addedVector instanceof Vector)) {
-      throw new Error('Error');
+      throw new Error('Error: должен быть передан объект типа Vector');
     }
 
-    // зачем try?
-    try {
-      // тут нужно вернуть новый вектор, старый мутировать не нужно
-      this.x += addedVector.x;
-      this.y += addedVector.y;
-      return new Vector(this.x, this.y);
-    }
-    catch (e) {
-      console.log(e);
-    }
-
+    return new Vector(this.x + addedVector.x, this.y + addedVector.y);
   }
 
   times(multiplier) {
 
     if (typeof multiplier !== 'number') {
-      throw `В качестве множителя должно быть число`;
+      throw new Error(`В качестве множителя должно быть число`);
     }
     // аналогично
-    try {
 
-      // лишняя мутация
-      this.x *= multiplier;
-      this.y *= multiplier;
+    // лишняя мутация
 
-      return new Vector(this.x, this.y);
-    }
-    catch (e) {
-      console.log(e);
-    }
+    return new Vector(this.x * multiplier, this.y * multiplier);
   }
+
 }
 
 // ************************************
@@ -55,70 +39,82 @@ class Actor {
     }
 
     // зачем тут try?
-    try {
-      this.pos = pos;
-      this.size = size;
-      this.speed = new Vector(speed.x, speed.y);
 
-      // свойства и методы нужно определять как поля класса
-      this.act = function () {
-      };
+    this.pos = pos;
+    this.size = size;
+    this.speed = new Vector(speed.x, speed.y);
 
-      Object.defineProperty(this, 'type', {
-        value: 'actor'
-      });
-      Object.defineProperty(this, 'left', {
-        get() {
-          return this.pos.x;
-        }
-      });
-      Object.defineProperty(this, 'top', {
-        get() {
-          return this.pos.y;
-        },
-        enumerable: true
-      });
-      Object.defineProperty(this, 'right', {
-        get() {
-          return this.pos.x + this.size.x;
-        },
-        enumerable: true
-      });
-      Object.defineProperty(this, 'bottom', {
-        get() {
-          return this.pos.y + this.size.y;
-        },
-        enumerable: true
+    // свойства и методы нужно определять как поля класса
+    /*this.act = function () {
+     };*/
 
-      });
-    }
-    catch (e) {
-      console.log(e);
-    }
+
+    /* Object.defineProperty(this, 'left', {
+     get() {
+     return this.pos.x;
+     }
+     });*/
+    /* Object.defineProperty(this, 'top', {
+     get() {
+     return this.pos.y;
+     },
+     enumerable: true
+     });
+     Object.defineProperty(this, 'right', {
+     get() {
+     return this.pos.x + this.size.x;
+     },
+     enumerable: true
+     });
+     Object.defineProperty(this, 'bottom', {
+     get() {
+     return this.pos.y + this.size.y;
+     },
+     enumerable: true
+
+     });*/
 
   }
 
+  act() {
+
+  };
+
+  get left() {
+    return this.pos.x;
+  }
+
+  get right() {
+    return this.pos.x + this.size.x;
+  }
+
+  get top() {
+    return this.pos.y;
+  }
+
+  get bottom() {
+    return this.pos.y + this.size.y;
+  }
+
+  get type() {
+    return 'actor';
+  }
 
   isIntersect(actorInstance) {
     // зачем делать left, right, top, bottom аргументами, если они передаются один раз?
-    const I = (a, b, x1, x2, y1, y2) => !(a[x2] <= b[x1] || a[x1] >= b[x2] || a[y2] <= b[y1] || a[y1] >= b[y2]);
+    const intersectionResult = (a, b) => !(a['right'] <= b['left'] || a['left'] >= b['right'] || a['bottom'] <= b['top'] || a['top'] >= b['bottom']);
 
     if (!(actorInstance instanceof Actor)) {
       throw new Error(`Вы не передали аргумент типа Actor`);
     }
 
     // зачем тут try?
-    try {
-      if (this === actorInstance) {
-        return false;
-      }
-      else {
-        return I(actorInstance, this, 'left', 'right', 'top', 'bottom');
 
-      }
+    if (this === actorInstance) {
+      return false;
     }
-    catch (e) {
-      console.log(e);
+    else {
+      return intersectionResult(actorInstance, this);
     }
   }
 
@@ -132,31 +128,26 @@ class Level {
     this.grid = gridMatrix.slice();
 
     // filter по-моему, лишняя проверка
-    this.actors = movingObjectsArray.slice().filter(item => item instanceof Object);
-
+    this.actors = movingObjectsArray.slice();
 
     // объявить через поля класса
-    Object.defineProperty(this, 'player', {
-      get() {
-        return this.actors.filter(item => item.type === 'player')[0]
-      },
-      enumerable: true,
-      configurable: true
-    });
-    Object.defineProperty(this, 'height', {
-      get() {
-        return (this.grid.every(item => Array.isArray(item))) ? this.grid.length : 1;
-      }
-    });
-    Object.defineProperty(this, 'width', {
-      get() {
-        let start = this.height > 1 ? this.grid[0].length : this.grid.length;
-        return this.grid.reduce((memo, item) => item.length > memo ? item.length : memo, start);
-      }
-    });
+
     this.status = null;
     this.finishDelay = 1;
 
+  }
+
+  get player() {
+    return this.actors.filter(item => item.type === 'player')[0]
+  }
+
+  get height() {
+    return (this.grid.every(item => Array.isArray(item))) ? this.grid.length : 1;
+  }
+
+  get width() {
+    let start = this.height > 1 ? this.grid[0].length : this.grid.length;
+    return this.grid.reduce((memo, item) => item.length > memo ? item.length : memo, start);
   }
 
   isFinished() {
@@ -169,17 +160,12 @@ class Level {
     }
 
     // зачем try?
-    try {
-      return this.actors.find(item => item.isIntersect(actorInstance));
-    }
-    catch (e) {
-      console.log(e);
-    }
+    return this.actors.find(item => item.isIntersect(actorInstance));
   }
 
   obstacleAt(objectPositionProspective, objectSize) {
     // тут достаточно последовательно проверить оба аргумента
-    if (arguments.length < 2 || [objectPositionProspective, objectSize].some(item => !item instanceof Vector)) {
+    if ([objectPositionProspective, objectSize].some(item => !item instanceof Vector)) {
       throw `Вы не передали объекты типа Vector`;
     }
     const borderCheck = (actorInstance) => {
@@ -195,24 +181,24 @@ class Level {
     };
 
     // зачем try?
-    try {
-      const virtualActor = new Actor(objectPositionProspective, objectSize);
-      const checkResult = borderCheck(virtualActor);
-      if (checkResult) {
-        return checkResult;
-      }
 
-      const searchRowInd = this.grid.findIndex((row, ind) => row.find((ceil, pos) =>
-        virtualActor.isIntersect(new Actor(new Vector(pos, ind)))));
+    const virtualActor = new Actor(objectPositionProspective, objectSize);
+    const checkResult = borderCheck(virtualActor);
+    if (checkResult) {
+      return checkResult;
+    }
 
-      // развернуть это выражение
-      return searchRowInd !== -1
-        ? this.grid[searchRowInd]
-          .find((ceil, pos) => virtualActor.isIntersect(new Actor(new Vector(pos, searchRowInd)))) : undefined;
+    const searchRowInd = this.grid.findIndex((row, ind) => row.find((ceil, pos) =>
+      virtualActor.isIntersect(new Actor(new Vector(pos, ind)))));
+
+    // развернуть это выражение
+    /* return searchRowInd !== -1
+     ? this.grid[searchRowInd]
+     .find((ceil, pos) => virtualActor.isIntersect(new Actor(new Vector(pos, searchRowInd)))) : undefined;*/
+    if (searchRowInd === -1) {
+      return undefined;
     }
-    catch (e) {
-      console.log(e);
-    }
+    return this.grid[searchRowInd].find((ceil, pos) => virtualActor.isIntersect(new Actor(new Vector(pos, searchRowInd))));
   }
 
   removeActor(actorInstance) {
@@ -224,7 +210,7 @@ class Level {
 
   noMoreActors(typeString) {
     // чтобы избежать отрицания можно использовать every()
-    return this.actors.length === 0 || !this.actors.some(item => item.type === typeString);
+    return this.actors.length === 0 || this.actors.every(item => item.type !== typeString);
   }
 
   playerTouched(typeObjectString, actorInstance) {
@@ -233,19 +219,15 @@ class Level {
     }
 
     // try
-    try {
-      if (['lava', 'fireball'].find(item => item === typeObjectString)) {
-        this.status = 'lost';
-      }
-      if (typeObjectString === 'coin' && actorInstance.type === 'coin') {
-        this.removeActor(actorInstance);
-        if (this.actors.find(item => item.type === 'coin')) {
-          this.status = 'won';
-        }
-      }
+
+    if (['lava', 'fireball'].find(item => item === typeObjectString)) {
+      this.status = 'lost';
     }
-    catch (e) {
-      console.log(e);
+    if (typeObjectString === 'coin' && actorInstance.type === 'coin') {
+      this.removeActor(actorInstance);
+      if (this.actors.find(item => item.type === 'coin')) {
+        this.status = 'won';
+      }
     }
 
   }
@@ -257,23 +239,23 @@ class Level {
 class LevelParser {
   constructor(objectsDictionary) {
     this.objectsDictionary = Object.assign({}, objectsDictionary);
-    console.log(this.objectsDictionary);
+
+    this.obstacleFromSymbol = function (symbolString) {
+      const OBSTACLES = {
+        '!': 'lava',
+        'x': 'wall'
+      };
+      return OBSTACLES[symbolString];
+    }
   }
 
   actorFromSymbol(symbolString) {
-    return this.objectsDictionary[symbolString];
+    return typeof this.objectsDictionary[symbolString] === 'function' ? this.objectsDictionary[symbolString] : undefined;
 
   }
 
-  obstacleFromSymbol(symbolString) {
-    // Это нужно вынести в поле класса (задать в конструкторе),
-    // иначе этот объект будет создаваться при каждом выхове метода
-    const OBSTACLES = {
-      '!': 'lava',
-      'x': 'wall'
-    };
-    return OBSTACLES[symbolString];
-  }
+  // Это нужно вынести в поле класса (задать в конструкторе),
+  // иначе этот объект будет создаваться при каждом выхове метода
 
   createGrid(stringsArr) {
 
@@ -285,7 +267,7 @@ class LevelParser {
     plan.forEach((item, ind) =>
       item.split('').forEach((el, pos) => {
         const testingConstructor = this.actorFromSymbol(el);
-        if (testingConstructor!== undefined && testingConstructor.prototype.constructor === testingConstructor) {
+        if (testingConstructor !== undefined && testingConstructor.prototype.constructor === testingConstructor) {
           actorsList.push(new testingConstructor(new Vector(pos, ind)));
         }
       }));
@@ -299,8 +281,81 @@ class LevelParser {
     return new Level(gridMatrix, actorsList);
   }
 
+}
+
+class Fireball extends Actor {
+  constructor(pos = new Vector(), speed = new Vector()) {
+    super();
+    this.pos = pos;
+    this.speed = new Vector(speed.x, speed.y);
+  }
+
+  get type() {
+    return 'fireball';
+  }
+
+  getNextPosition(time = 1) {
+    return new Vector(this.pos.x + time * this.speed.x, this.pos.y + time * this.speed.y);
+  }
+
+  handleObstacle() {
+    this.speed.x = -this.speed.x;
+    this.speed.y = -this.speed.y;
+  }
+
+  act(time, levelObj) {
+    const nextPos = this.getNextPosition(time);
+    if (levelObj.obstacleAt(nextPos, this.size)) {
+      this.handleObstacle();
+    }
+    else {
+      this.pos = nextPos;
+    }
+  }
+}
+
+class HorizontalFireball extends Fireball {
+  constructor(pos) {
+    super();
+    this.pos = pos;
+    this.speed = new Vector(2, 0);
+  }
 
 }
+
+class VerticalFireball extends Fireball {
+  constructor(pos) {
+    super();
+    this.pos = pos;
+    this.speed = new Vector(0, 2);
+  }
+}
+
+class FireRain extends Fireball {
+  constructor(pos) {
+    super();
+    this.pos = pos;
+    this.initialPos = pos;
+    this.speed = new Vector(0,3);
+  }
+
+  handleObstacle() {
+    this.pos = new Vector(this.initialPos.x, this.initialPos.y);
+  }
+
+  act(time, levelObj) {
+    const nextPos = this.getNextPosition(time);
+    if (levelObj.obstacleAt(nextPos, this.size)) {
+      this.handleObstacle();
+    }
+    else {
+      this.pos = nextPos;
+    }
+  }
+}
+
+/*console.log(Fireball.prototype.constructor, Object.getPrototypeOf(Fireball.prototype), Object.getPrototypeOf(HorizontalFireball.prototype),
+  Actor.prototype.isPrototypeOf(Level.prototype));*/
 
 
 
